2af93b6680b703e387e9e05104695ac788a43be7
bharat-kbsp <bharat.kargathiya@bsptechno.com>
2025-03-31 14:41:52 +0530

    automation build MAC 14

---
+++ lib/flavor_config.dart
@@ -0,0 +1,8 @@
+class FlavorConfig {
+  /// Flavor Identifier is the same as flavor names in build_flavor.gradle file
+  final String flavorId;
+
+  FlavorConfig({
+    required this.flavorId,
+  });
+}
